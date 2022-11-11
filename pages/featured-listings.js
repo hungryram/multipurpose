75932d3f53744a252c4954272f9d47feb2b38ce4
Ram Dettmer <ramdettmer751@gmail.com>
2022-11-10 17:34:46 -0800

    add featured listings

--- pages/featured-listings.js
+++ pages/featured-listings.js
@@ -7,17 +7,7 @@ import Section from "../components/util/section"
 export default function FeaturedListings() {
     return (
         <>
-        <Head>
-            <script src="https://kestrel.idxhome.com/ihf-kestrel.js" />
-            <script
-                dangerouslySetInnerHTML={{
-                    __html: `window.ihfKestrel = window.ihfKestrel || { };
-                    ihfKestrel.config = {
-                        platform: "custom",
-                    activationToken: "3779C949-155D-6043-0911FD9A208A875D",};`
-                }}
-            />
-        </Head>
+
         <Layout>
             <Section>
                 <Container>
