ae6c9bb209c7b83a25a035297a893f205445c431
Ram Dettmer <ramdettmer751@gmail.com>
2022-11-10 17:45:41 -0800

    add featured listings

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,15 @@ export default class Document extends ServerStyleSheetDocument {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
-
+          <script src="https://kestrel.idxhome.com/ihf-kestrel.js" />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `window.ihfKestrel = window.ihfKestrel || { };
+                        ihfKestrel.config = {
+                            platform: "custom",
+                        activationToken: "3779C949-155D-6043-0911FD9A208A875D",};`
+                    }}
+                />
         </Head>
         <body>
           <Main />
