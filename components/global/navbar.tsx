08c64036bbb2858f10fc42aab9f5bb6791a68898
Ram Dettmer <ramdettmer751@gmail.com>
2022-11-29 21:07:17 -0800

    add ada

--- components/global/navbar.tsx
+++ components/global/navbar.tsx
@@ -223,7 +223,7 @@ export default function Navbar({
                                     id="toggle"
                                     className="cursor-pointer flex justify-end z-50"
                                     onClick={openMobileNav ? () => setOpenMobileNav(false) : () => setOpenMobileNav(true)}
-                                    aria-expanded={openMobileNav ? 'false' : 'true'}
+                                    // aria-expanded={openMobileNav ? 'true' : 'false'}
                                 >
                                     <HamburgerMenu
                                         isOpen={openMobileNav}
