a7928fb05f42e703b7d775b663077cb81e63bd7b
Ram Dettmer <ramdettmer751@gmail.com>
2022-12-06 12:22:16 -0800

    update icon section

--- components/util/body-text.tsx
+++ components/util/body-text.tsx
@@ -1,30 +1,32 @@
 import ContentEditor from "../templates/contenteditor";
 import PrimaryButton from "./primary-button";
 
-export default function BodyText({ 
+export default function BodyText({
     heading,
     body,
     bodyStyle,
-    fullWidth, 
-    headerStyle, 
-    textAlign, 
-    buttonText, 
-    buttonLink, 
-    buttonBackground, 
-    buttonTextColor 
+    fullWidth,
+    headerStyle,
+    textAlign,
+    buttonText,
+    buttonLink,
+    buttonBackground,
+    buttonTextColor
 }: any) {
     return (
         <div className="md:flex justify-center py-6">
-            <div className={fullWidth ? 'w-full' : 'md:w-3/4'}>
-                <div className={`px-4 ${textAlign ? 'text-left': 'text-center'}`}>
-                    {heading && <h2 className="h2 mb-6" style={headerStyle}>{heading}</h2>}
-                    {body &&
-                        <div className="content" style={bodyStyle}>
-                            <ContentEditor
-                                content={body}
-                            />
-                        </div>
-                    }
+            <div className={fullWidth ? 'w-full' : 'md:w-3/5'}>
+                <div className={`px-4 ${textAlign ? 'text-left' : 'text-center'}`}>
+                    <div className="content">
+                        {heading && <h2 className="h2 mb-6" style={headerStyle}>{heading}</h2>}
+                        {body &&
+                            <div className="content" style={bodyStyle}>
+                                <ContentEditor
+                                    content={body}
+                                />
+                            </div>
+                        }
+                    </div>
                     {buttonLink &&
                         <div className="mt-10">
                             <PrimaryButton
