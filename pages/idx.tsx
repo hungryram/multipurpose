c54e0b4cfa7d517ed44f9b7f4c513a45af534938
Ram Dettmer <ramdettmer751@gmail.com>
2023-04-05 18:03:37 -0700

    idx

--- pages/idx.tsx
+++ pages/idx.tsx
@@ -26,11 +26,6 @@ const IhomefinderEmbed = () => {
 const IhomefinderPage = () => {
     return (
         <>
-            <Head>
-                <title>My iHomefinder Page</title>
-                <script src="https://kestrel.idxhome.com/ihf-kestrel.js"></script>
-                <script dangerouslySetInnerHTML={{ __html: `window.ihfKestrel = window.ihfKestrel || {}; ihfKestrel.config = { platform: "custom", activationToken: "3779C949-155D-6043-0911FD9A208A875D" };` }} />
-            </Head>
             <Layout>
                 <h1>IDX TEST wlayout</h1>
                 <IhomefinderEmbed />
