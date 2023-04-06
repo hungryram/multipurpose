4cda98eaab8d50b6d40cf8288d576e6cf0d7dcb7
Ram Dettmer <ramdettmer751@gmail.com>
2023-04-05 17:52:19 -0700

    idx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,16 +9,6 @@ export default class Document extends ServerStyleSheetDocument {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
-          <script src="https://kestrel.idxhome.com/ihf-kestrel.js" />
-          <script dangerouslySetInnerHTML={{
-            __html: `<script>
-            window.ihfKestrel = window.ihfKestrel || {};
-            ihfKestrel.config = {
-              platform: "custom",
-              activationToken: "3779C949-155D-6043-0911FD9A208A875D",
-            };
-          </script>`
-          }}/>
         </Head>
         <body>
           <Main />
