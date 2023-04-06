c2c86bf8e33365dd03a61c349fdfaa547d9d1bce
Ram Dettmer <ramdettmer751@gmail.com>
2023-04-06 01:41:25 -0700

    test idx_body

--- pages/idx.tsx
+++ pages/idx.tsx
@@ -25,12 +25,12 @@ export async function getStaticProps({ preview = false }) {
 
     /* when the client isn't set up */
     return {
-        props: { idx_body },
+        props: {idx_body},
         revalidate: undefined,
     }
 }
 
-export default function IHomefinderEmbed({ appearanceQuery, idx_body }) {
+export default function IHomefinderEmbed({appearanceQuery, idx_body}) {
     // useEffect(() => {
     //     // Load the iHomefinder embed code on the client side
     //     const script = document.createElement('script');
@@ -43,22 +43,45 @@ export default function IHomefinderEmbed({ appearanceQuery, idx_body }) {
     //     };
     // }, []);
 
-    const FooterComponent = () => {
-        return (
-            <div style={{ position: "fixed", bottom: 0, width: "100%", backgroundColor: "white" }}>
-                <footer className={Styles.footer}>
-                    <div className="pt-20 pb-10">
-                        <h1>Tet</h1>
-                    </div>
-                </footer>
-            </div>
-        )
-    }
-
-    console.log(appearanceQuery?.appearances?.branding?.logo?.asset?.url)
+console.log(appearanceQuery?.appearances?.branding?.logo?.asset?.url)
     return (
         <>
+            <Head>
+                <style>
+                    {`
+                        :root {
 
+                            --primary-accent: ${appearanceQuery?.appearances?.primaryAccent ?? '#cccccc'};
+
+                            --footer-background-color: ${appearanceQuery?.appearances?.footerBg ?? '#0d1321'};
+                            --footer-header-color: ${appearanceQuery?.appearances?.footerHeader ?? '#ffffff'};
+                            --footer-text-color: ${appearanceQuery?.appearances?.footerText ?? '#9b9b9b'};
+                            --primary-button-background: ${appearanceQuery?.appearances?.primaryButtonBg ?? 'transparent'};
+                            --primary-button-text: ${appearanceQuery?.appearances?.primaryButtonText ?? '#000000'};
+                            --secondary-button-background: ${appearanceQuery?.appearances?.secondaryButtonBg ?? 'transparent'};
+                            --secondary-button-text: ${appearanceQuery?.appearances?.secondaryButtonText ?? '#cccccc'};
+                            --secondary-color: ${appearanceQuery?.appearances?.secondaryColor ?? '#cccccc'};
+
+                            --header-background-color: ${appearanceQuery?.appearances?.navBgColor ? appearanceQuery?.appearances?.navBgColor : 'transparent'};
+                            --header-navigation-color: ${appearanceQuery?.appearances?.navColor ?? '#ffffff'};
+                            --mobile-icon-color: ${appearanceQuery?.appearances?.mobileIconColor ?? '#ffffff'};
+
+                            --loading-background-color: ${appearanceQuery?.appearances?.loaderColor ?? '#0e0e0e'};
+                            --loading-image: url(${appearanceQuery?.appearances?.loaderImage});
+
+                            --website-body-color: ${appearanceQuery?.appearances?.websiteBodyColor ?? '#fff'};
+                            --website-text-color: ${appearanceQuery?.appearances?.websiteTextColor ?? '#222'};
+
+                            --button-radius: ${`${appearanceQuery?.appearances?.buttonRadius ?? 4}px`};
+                            --button-y-padding: ${`${appearanceQuery?.appearances?.buttonYPadding ?? 16}px`};
+                            --button-x-padding: ${`${appearanceQuery?.appearances?.buttonXPadding ?? 50}px`};
+                            
+                            --announcementbar-background-color: ${appearanceQuery?.appearances?.announcementBar?.announcementBgColor};
+                            --announcementbar-text-color: ${appearanceQuery?.appearances?.announcementBar?.announcementTextColor};
+                        }
+                    `}
+                </style>
+            </Head>
             <Navbar
                 company_name={appearanceQuery?.profileSettings?.company_name}
                 logo={appearanceQuery?.appearances?.branding?.logo?.asset?.url}
@@ -81,7 +104,6 @@ export default function IHomefinderEmbed({ appearanceQuery, idx_body }) {
             />
             <Header
             />
-
             <div className="section">
                 <div className="container">
                     <div className="idxTest">
@@ -90,10 +112,11 @@ export default function IHomefinderEmbed({ appearanceQuery, idx_body }) {
                         {/* <div dangerouslySetInnerHTML={{ __html: '<script>document.currentScript.replaceWith(ihfKestrel.render());</script>' }} /> */}
                         <div dangerouslySetInnerHTML={{
                             __html: idx_body
-                        }} />
+                        }}/>
                     </div>
                 </div>
             </div>
+
             <Footer
                 footerText={appearanceQuery?.appearances?.footer?.footerText}
                 image={appearanceQuery?.appearances?.footer?.footerLogo}
