cbc1be2feff3167e8241e6ac1a15242f3bcca046
Ram Dettmer <ramdettmer751@gmail.com>
2023-04-05 18:24:43 -0700

    idx

--- pages/idx.tsx
+++ pages/idx.tsx
@@ -1,31 +1,44 @@
 // pages/ihomefinder.js
 import Head from 'next/head';
 import { useEffect } from 'react';
-import Layout from '../components/global/layout';
+import dynamic from 'next/dynamic';
 
-const IhomefinderPage = () => {
-    useEffect(() => {
-        // Load the iHomefinder embed code on the client side
-        const script = document.createElement('script');
-        script.async = true;
-        document.body.appendChild(script);
+const Layout = dynamic(() => import('../components/global/layout'));
 
-        // Replace the script tag with the iHomefinder embed code
-        script.onload = () => {
-            document.currentScript.replaceWith(ihfKestrel.render());
-        };
-    }, []);
+const IhomefinderEmbed = () => {
+  useEffect(() => {
+    // Load the iHomefinder embed code on the client side
+    const script = document.createElement('script');
+    script.async = true;
+    document.body.appendChild(script);
 
-    return (
-        <>
-            <h1>IDX TEST wlayout</h1>
-            <div className="idxTest">
-                <div dangerouslySetInnerHTML={{ __html: '<script>document.currentScript.replaceWith(ihfKestrel.render());</script>' }} />
-            </div>
-        </>
-    );
+    // Replace the script tag with the iHomefinder embed code
+    script.onload = () => {
+      document.currentScript.replaceWith(ihfKestrel.render());
+    };
+  }, []);
+
+  return (
+    <div className="idxTest">
+      <script dangerouslySetInnerHTML={{ __html: 'document.currentScript.replaceWith(ihfKestrel.render());' }} />
+    </div>
+  );
 };
 
-IhomefinderPage.getInitialProps = async () => ({});
+const IhomefinderPage = () => {
+  return (
+    <>
+      <Head>
+        <title>My iHomefinder Page</title>
+        <script src="https://kestrel.idxhome.com/ihf-kestrel.js"></script>
+        <script dangerouslySetInnerHTML={{ __html: `window.ihfKestrel = window.ihfKestrel || {}; ihfKestrel.config = { platform: "custom", activationToken: "3779C949-155D-6043-0911FD9A208A875D" };` }} />
+      </Head>
+      <Layout>
+        <h1>IDX TEST wlayout</h1>
+        <IhomefinderEmbed />
+      </Layout>
+    </>
+  );
+};
 
 export default IhomefinderPage;
