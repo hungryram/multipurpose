a1972c658aef49f4f3e96c7102de4a04a2abfaed
Ram Dettmer <ramdettmer751@gmail.com>
2023-04-05 22:48:12 -0700

    working idx

--- pages/idx.tsx
+++ pages/idx.tsx
@@ -15,8 +15,10 @@ export async function getStaticProps({ preview = false }) {
     if (process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
         const appearanceQuery = await getClient(false).fetch(appearances)
 
+        const idx_body = `{idx_body}`
+
         return {
-            props: { preview, appearanceQuery },
+            props: { preview, appearanceQuery, idx_body },
             // If webhooks isn't setup then attempt to re-generate in 1 minute intervals
             revalidate: process.env.SANITY_REVALIDATE_SECRET ? undefined : 60,
         }
@@ -29,7 +31,7 @@ export async function getStaticProps({ preview = false }) {
     }
 }
 
-export default function IHomefinderEmbed(appearanceQuery) {
+export default function IHomefinderEmbed({appearanceQuery, idx_body}) {
     const router = useRouter();
     useEffect(() => {
         // Load the iHomefinder embed code on the client side
