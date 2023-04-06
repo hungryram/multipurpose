d27650341f9017f267152b7569c1c3700ba59054
Ram Dettmer <ramdettmer751@gmail.com>
2023-04-06 00:27:06 -0700

    test idx_body

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,18 @@
 import Head from 'next/head'
-import Layout from '../components/global/layout'
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
+      {/* <Head>
         <script src="https://kestrel.idxhome.com/ihf-kestrel.js"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `window.ihfKestrel = window.ihfKestrel || {}; ihfKestrel.config = { platform: "custom", activationToken: "3779C949-155D-6043-0911FD9A208A875D" };`,
           }}
         />
-        <script src="/jquery.min.js"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-    jQuery.noConflict();
-    jQuery(document).ready(function () {
-      alert('The DOM is ready');
-    });
-  `}}></script>
-      </Head>
+        
+      </Head> */}
       <Component {...pageProps} />
     </>
   )
