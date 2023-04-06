759d33abff8b967f56c3c2b0908cf1b236bc461d
Ram Dettmer <ramdettmer751@gmail.com>
2023-04-05 17:27:45 -0700

    idx

--- pages/idx.tsx
+++ pages/idx.tsx
@@ -1,33 +1,36 @@
 // pages/ihomefinder.js
 import Head from 'next/head';
 import { useEffect } from 'react';
+import Layout from '../components/global/layout';
 
 const IhomefinderPage = () => {
-  useEffect(() => {
-    // Load the iHomefinder embed code on the client side
-    const script = document.createElement('script');
-    script.async = true;
-    document.body.appendChild(script);
+    useEffect(() => {
+        // Load the iHomefinder embed code on the client side
+        const script = document.createElement('script');
+        script.async = true;
+        document.body.appendChild(script);
 
-    // Replace the script tag with the iHomefinder embed code
-    script.onload = () => {
-      document.currentScript.replaceWith(ihfKestrel.render());
-    };
-  }, []);
+        // Replace the script tag with the iHomefinder embed code
+        script.onload = () => {
+            document.currentScript.replaceWith(ihfKestrel.render());
+        };
+    }, []);
 
-  return (
-    <>
-      <Head>
-        <script src="https://kestrel.idxhome.com/ihf-kestrel.js"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.ihfKestrel = window.ihfKestrel || {}; ihfKestrel.config = { platform: "custom", activationToken: "3779C949-155D-6043-0911FD9A208A875D" };`,
-          }}
-        />
-      </Head>
-      <div dangerouslySetInnerHTML={{ __html: '<script>document.currentScript.replaceWith(ihfKestrel.render());</script>' }} />
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <script src="https://kestrel.idxhome.com/ihf-kestrel.js"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `window.ihfKestrel = window.ihfKestrel || {}; ihfKestrel.config = { platform: "custom", activationToken: "3779C949-155D-6043-0911FD9A208A875D" };`,
+                    }}
+                />
+            </Head>
+            <Layout>
+                <div dangerouslySetInnerHTML={{ __html: '<script>document.currentScript.replaceWith(ihfKestrel.render());</script>' }} />
+            </Layout>
+        </>
+    );
 };
 
 IhomefinderPage.getInitialProps = async () => ({});
