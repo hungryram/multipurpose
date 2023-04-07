8c9c8ecae005c6e038605679a092b3e7e0c2003f
Ram Dettmer <ramdettmer751@gmail.com>
2023-04-07 00:30:50 -0700

    update

--- pages/idx.js
+++ pages/idx.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import Navbar from '../components/global/navbar';
 import Footer from '../components/global/footer';
 import { getClient } from '../lib/sanity.server';
@@ -25,63 +25,24 @@ export async function getStaticProps({ preview = false }) {
 
     /* when the client isn't set up */
     return {
-        props: {idx_body},
+        props: { idx_body },
         revalidate: undefined,
     }
 }
 
-export default function IHomefinderEmbed({appearanceQuery, idx_body}) {
-    // useEffect(() => {
-    //     // Load the iHomefinder embed code on the client side
-    //     const script = document.createElement('script');
-    //     script.async = true;
-    //     document.body.appendChild(script);
+export default function IHomefinderEmbed({ appearanceQuery, idx_body }) {
+    const iframeRef = useRef();
 
-    //     // Replace the script tag with the iHomefinder embed code
-    //     script.onload = () => {
-    //         document.currentScript.replaceWith(ihfKestrel.render());
-    //     };
-    // }, []);
-
-console.log(appearanceQuery?.appearances?.branding?.logo?.asset?.url)
+    useEffect(() => {
+        // Load the iHomefinder embed code in the iframe
+        const iframe = iframeRef.current;
+        const doc = iframe.contentDocument || iframe.contentWindow.document;
+        doc.open();
+        doc.write(idx_body);
+        doc.close();
+    }, []);
     return (
         <>
-            <Head>
-                <style>
-                    {`
-                        :root {
-
-                            --primary-accent: ${appearanceQuery?.appearances?.primaryAccent ?? '#cccccc'};
-
-                            --footer-background-color: ${appearanceQuery?.appearances?.footerBg ?? '#0d1321'};
-                            --footer-header-color: ${appearanceQuery?.appearances?.footerHeader ?? '#ffffff'};
-                            --footer-text-color: ${appearanceQuery?.appearances?.footerText ?? '#9b9b9b'};
-                            --primary-button-background: ${appearanceQuery?.appearances?.primaryButtonBg ?? 'transparent'};
-                            --primary-button-text: ${appearanceQuery?.appearances?.primaryButtonText ?? '#000000'};
-                            --secondary-button-background: ${appearanceQuery?.appearances?.secondaryButtonBg ?? 'transparent'};
-                            --secondary-button-text: ${appearanceQuery?.appearances?.secondaryButtonText ?? '#cccccc'};
-                            --secondary-color: ${appearanceQuery?.appearances?.secondaryColor ?? '#cccccc'};
-
-                            --header-background-color: ${appearanceQuery?.appearances?.navBgColor ? appearanceQuery?.appearances?.navBgColor : 'transparent'};
-                            --header-navigation-color: ${appearanceQuery?.appearances?.navColor ?? '#ffffff'};
-                            --mobile-icon-color: ${appearanceQuery?.appearances?.mobileIconColor ?? '#ffffff'};
-
-                            --loading-background-color: ${appearanceQuery?.appearances?.loaderColor ?? '#0e0e0e'};
-                            --loading-image: url(${appearanceQuery?.appearances?.loaderImage});
-
-                            --website-body-color: ${appearanceQuery?.appearances?.websiteBodyColor ?? '#fff'};
-                            --website-text-color: ${appearanceQuery?.appearances?.websiteTextColor ?? '#222'};
-
-                            --button-radius: ${`${appearanceQuery?.appearances?.buttonRadius ?? 4}px`};
-                            --button-y-padding: ${`${appearanceQuery?.appearances?.buttonYPadding ?? 16}px`};
-                            --button-x-padding: ${`${appearanceQuery?.appearances?.buttonXPadding ?? 50}px`};
-                            
-                            --announcementbar-background-color: ${appearanceQuery?.appearances?.announcementBar?.announcementBgColor};
-                            --announcementbar-text-color: ${appearanceQuery?.appearances?.announcementBar?.announcementTextColor};
-                        }
-                    `}
-                </style>
-            </Head>
             <Navbar
                 company_name={appearanceQuery?.profileSettings?.company_name}
                 logo={appearanceQuery?.appearances?.branding?.logo?.asset?.url}
@@ -110,9 +71,11 @@ console.log(appearanceQuery?.appearances?.branding?.logo?.asset?.url)
                         <h1>Test IDX BODY</h1>
                         {/* First div is for version 10 */}
                         {/* <div dangerouslySetInnerHTML={{ __html: '<script>document.currentScript.replaceWith(ihfKestrel.render());</script>' }} /> */}
-                        <div dangerouslySetInnerHTML={{
-                            __html: idx_body
-                        }}/>
+                        <iframe
+                            title="IDX Frame"
+                            ref={iframeRef}
+                            style={{ width: '100%', height: '800px', border: 'none' }}
+                        />
                     </div>
                 </div>
             </div>
@@ -123,43 +86,6 @@ console.log(appearanceQuery?.appearances?.branding?.logo?.asset?.url)
                     <h1>FOOTER</h1>
                 </div>
             </div>
-
-            {/* <Footer
-                footerText={appearanceQuery?.appearances?.footer?.footerText}
-                image={appearanceQuery?.appearances?.footer?.footerLogo}
-                hours={appearanceQuery?.appearances?.footer?.hours}
-                monday={appearanceQuery?.profileSettings?.hours?.monday}
-                tuesday={appearanceQuery?.profileSettings?.hours?.tuesday}
-                wednesday={appearanceQuery?.profileSettings?.hours?.wednesday}
-                thursday={appearanceQuery?.profileSettings?.hours?.thursday}
-                friday={appearanceQuery?.profileSettings?.hours?.friday}
-                saturday={appearanceQuery?.profileSettings?.hours?.saturday}
-                sunday={appearanceQuery?.profileSettings?.hours?.sunday}
-                company_name={appearanceQuery?.profileSettings?.company_name}
-                legal={appearanceQuery?.legal}
-                email={appearanceQuery?.profileSettings?.contact_information?.email}
-                phone_number={appearanceQuery?.profileSettings?.contact_information?.phone_number}
-                office_number={appearanceQuery?.profileSettings?.contact_information?.office_number}
-                website={appearanceQuery?.profileSettings?.settings?.websiteName}
-                address={appearanceQuery?.profileSettings?.address?.address}
-                city={appearanceQuery?.profileSettings?.address?.city}
-                state={appearanceQuery?.profileSettings?.address?.state}
-                zip_code={appearanceQuery?.profileSettings?.address?.zip_code}
-                links={appearanceQuery?.appearances?.footer?.quickLinks}
-                googleBusiness={appearanceQuery?.profileSettings?.social?.googleBusiness}
-                facebook={appearanceQuery?.profileSettings?.social?.facebook}
-                youtube={appearanceQuery?.profileSettings?.social?.youtube}
-                instagram={appearanceQuery?.profileSettings?.social?.instagram}
-                twitter={appearanceQuery?.profileSettings?.social?.twitter}
-                reddit={appearanceQuery?.profileSettings?.social?.reddit}
-                linkedin={appearanceQuery?.profileSettings?.social?.linkedin}
-                yelp={appearanceQuery?.profileSettings?.social?.yelp}
-                pinterest={appearanceQuery?.profileSettings?.social?.pinterest}
-                tiktok={appearanceQuery?.profileSettings?.social?.tiktok}
-                zillow={appearanceQuery?.profileSettings?.social?.zillow}
-                size={appearanceQuery?.profileSettings?.social?.size}
-                footerDisclaimer={appearanceQuery?.appearances?.footer?.footerDisclaimer}
-            /> */}
         </>
     );
 };
