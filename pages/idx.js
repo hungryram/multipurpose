a91494cad7628ec12ac9c6b7cfd3780ac2209039
Ram Dettmer <ramdettmer751@gmail.com>
2023-04-18 15:28:57 -0700

    tried new idx

--- pages/idx.js
+++ pages/idx.js
@@ -1,18 +1,31 @@
+import { useEffect } from 'react';
+import Navbar from '../components/global/navbar';
+import Footer from '../components/global/footer';
+import { getClient } from '../lib/sanity.server';
+import { appearances } from '../lib/queries';
+import Head from 'next/head';
+import Header from '../components/templates/header';
 
 export const getStaticProps = async () => {
+
+    const appearanceQuery = await getClient(false).fetch(appearances)
+    const idxBody = `{idx_body}`
   
     return {
-      props: {
-        idx_body: `{idx_body}`,
-      },
+      props: {appearanceQuery, idxBody}
     };
   };
   
-  const MyPage = ({ idx_body }) => {
+  const MyPage = ({ idxBody, appearanceQuery }) => {
     return (
       <div>
         <h1>IDX BODY</h1>
-        {idx_body}
+        {idxBody}
+        <div className="mt-10">
+            <div className="bg-black text-white">
+                <h1>footer</h1>
+            </div>
+        </div>
       </div>
     );
   };
