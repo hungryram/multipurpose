3e9ceec31ec6334771a4f973e8a12cdfbd20b4d6
Ram Dettmer <ramdettmer751@gmail.com>
2023-04-18 12:39:50 -0700

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   assetPrefix: 'https://multipurpose-inky.vercel.app',
-  polyfill: false,
   swcMinify: false,
   images: {
     remotePatterns: [
